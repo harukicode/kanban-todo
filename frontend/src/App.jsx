2ea82fc091374919292a1fd1177a2881c806f786
harukicode <sparky.doto@gmail.com>
2024-10-27 12:19:33 +0100

    refactored sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar.jsx'
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -5,15 +6,18 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-import Sidebar from "../components/SideBar/Sidebar.jsx";
-import KanbanBoard from "../components/KanbanBoard/KanbanBoard.jsx";
+import KanbanBoard from "./components/KanbanBoard/KanbanBoard.jsx";
 import "./App.css";
+import SideBarS from "./components/SideBar/SideBarS.jsx";
+
 const App = () => {
   return (
     <Router>
       <div className="flex">
-        <Sidebar />
+        <SidebarProvider>
+        <SideBarS />
         <main className="flex-grow p-4">
+          <SidebarTrigger />
           <Routes>
             <Route path="/" element={<Navigate to="/kanban" />} />
             <Route path="/kanban" element={<KanbanBoard />} />
@@ -21,6 +25,7 @@ const App = () => {
             <Route path="/notes" element={<div>Notes Component</div>} />
           </Routes>
         </main>
+        </SidebarProvider>
       </div>
     </Router>
   );
