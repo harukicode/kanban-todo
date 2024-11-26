f863f6c99e5d655d7fc8561b860f9a97c73d806f
harukicode <sparky.doto@gmail.com>
2024-11-26 08:41:15 +0100

    ref color tasks

--- frontend/src/hooks/Task/usePriorityColor.jsx
+++ frontend/src/hooks/Task/usePriorityColor.jsx
@@ -5,10 +5,10 @@ export const usePriorityColor = (priority) => {
     switch (priority.toLowerCase()) {
       case "high":
         return "bg-red-100";
-      case "secondary":
-        return "bg-blue-100";
+      case "medium":
+        return "bg-orange-100";
       case "low":
-        return "bg-yellow-100";
+        return "bg-blue-100";
       default:
         return "bg-gray-100";
     }
